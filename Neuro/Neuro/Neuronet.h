f85cae07eac3796fd253fd1ade7250c501e86250
Владислав Пельтихин <peltikhin.vladik@gmail.com>
2019-06-10 16:37:12 +0300

    Update Neuronet.h

--- Neuro/Neuro/Neuronet.h
+++ Neuro/Neuro/Neuronet.h
@@ -5,6 +5,10 @@ class Neuronet
 public:
 	Neuronet(vector<int>);
 	~Neuronet();
+	vector<double> calculate(vector<double> input);
+	void learning(vector<double> input, vector<double> trueOutput);
+	void save(string);
+	void load(string);
 private:
 	vector<MatrixOfWeights> matrixsOfWeights;
 };
